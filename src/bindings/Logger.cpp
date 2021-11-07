a27f39ea98e52dd1c73723ac6538830e9f0426d5
HuxyUK <HuxyUK@users.noreply.github.com>
2021-11-07 21:40:02 +0000

    ✨ [Logger]: Custom logger for PyASGE
    ASGE uses a logger that has a number of predefined styles and
    levels. The "loggable" level is set by the build type i.e. Release
    or Debug. As the DLL bundled with PyASGE is a Release build, any
    calls to the logger above ERROR are automatically discared. To
    circumvent this a custom logger has been added to PyASGE, that does
    not obey the level limits. This allows for easy logging to be done
    in game and to the std out with the minimum of fuss.

--- src/bindings/Logger.cpp
+++ src/bindings/Logger.cpp
@@ -16,10 +16,58 @@
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
+/// \brief   A logger that doesn't discriminate between log levels
+/// \details The standard ASGE logger uses DEBUG/RELEASE builds to determine
+///          which log levels should be logged. This replacement drop-in uses
+///          a custom log function instead that doesn't perform a log level
+///          check, thus allowing all log levels to work.
+
+class PYASGELogger : public Logging::LoggerBase
+{
+public:
+    PYASGELogger() = delete;
+
+    explicit PYASGELogger(const Logging::LoggingConfig_t& config) :
+            LoggerBase(config), levels(config.find("color") != config.end() ? colored : uncolored)
+    {
+
+    }
+
+    void log(const std::string &message, Logging::LogLevel level) override
+    {
+      std::string output;
+      output.reserve(message.length() + 64);
+      output.append(Logging::timestamp());
+      output.append(levels.find(level)->second);
+      output.append(message);
+      output.push_back('\n');
+      log(output);
+    }
+
+    void log(const std::string &message) override
+    {
+      std::cout << message;
+      std::cout.flush();
+    }
+
+protected:
+    const std::unordered_map<Logging::LogLevel, std::string, Logging::EnumHasher> levels; //NOLINT
+};
+
 void initLogger(py::module_ &logger) {
-  logger.def("INFO", &Logging::INFO);
-  logger.def("ERROR", &Logging::ERRORS);
-  logger.def("WARN", &Logging::WARN);
-  logger.def("DEBUG", &Logging::DEBUG);
-  logger.def("TRACE", &Logging::TRACE);
+
+  const Logging::LoggingConfig_t& config = { { "type", "pyasge" }, { "color", "" } };
+
+  [[maybe_unused]] const bool pyasge_logger_registered =
+          Logging::RegisterLogger("pyasge", [](const Logging::LoggingConfig_t &config) {
+              Logging::LoggerBase *logger = new PYASGELogger(config); //NOLINT
+              return logger;
+          });
+
+  logger.def("INFO", [config](const std::string& msg) { Logging::getLogger(config).log(msg, Logging::LogLevel::INFO); });
+  logger.def("ERROR",[config](const std::string& msg) { Logging::getLogger(config).log(msg, Logging::LogLevel::ERRORS); });
+  logger.def("WARN", [config](const std::string& msg) { Logging::getLogger(config).log(msg, Logging::LogLevel::WARN); });
+  logger.def("DEBUG",[config](const std::string& msg) { Logging::getLogger(config).log(msg, Logging::LogLevel::DEBUG); });
+  logger.def("TRACE",[config](const std::string& msg) { Logging::getLogger(config).log(msg, Logging::LogLevel::TRACE); });
+  logger.def("LOG",  [config](const std::string& msg) { Logging::getLogger(config).log(Logging::timestamp() + " [LOG] " + msg + "\n"); });
 }
