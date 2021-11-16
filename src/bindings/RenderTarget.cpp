c9abd35cc5cd9165e847fbf449fd43afe507c95a
HuxyUK <HuxyUK@users.noreply.github.com>
2021-11-16 00:09:50 +0000

    ✨ [RenderTarget]: Support for MSAA render targets
    As the render targets for ASGE have been changed to make use of
    multi-sampling, some small changes were required to the bindings.
    The most relevant is that of the function call `resolve`. This is
    used to convert the multi-sampled textures in to a standard 2D
    texture, suitable for attachment in a shader. At present there is
    no way to manually specify the sampling levels, though it can be
    turned off globally using `GameSettings`.

--- src/bindings/RenderTarget.cpp
+++ src/bindings/RenderTarget.cpp
@@ -35,6 +35,20 @@ void initRenderTarget(py::module_& module)
     ----
     In order to use a render target a renderer is needed. This is because the
     setup of such targets is GPU and framework dependent.
+
+    Example
+    -------
+      >>> # Render Function
+      >>> """ render to texture """
+      >>> self.renderer.setRenderTarget(self.rt)
+      >>> self.renderer.viewport = pyasge.Viewport(0, 0, 1024 / 2, 768 / 2)
+      >>> self.renderer.render(self.sprite)
+      >>>
+      >>> """ render the resultant texture """
+      >>> self.renderer.setRenderTarget(None)
+      >>> self.rt.resolve(0)
+      >>> self.renderer.viewport = pyasge.Viewport(0, 0, 1024, 768)
+      >>> self.renderer.render(self.rt_texture)
   )")
 
     .def(
@@ -45,9 +59,105 @@ void initRenderTarget(py::module_& module)
       py::arg("format"),
       py::arg("count"))
 
-    .def_property(
-      "buffers",
-      &ASGE::GLRenderTarget::getBuffers,
-      &ASGE::GLRenderTarget::setBuffers,
-      "Access to the textures associated with this render target.");
+      .def_property_readonly(
+          "buffers",
+          [](const ASGE::GLRenderTarget &self) {
+            auto &buffers = self.getResolved();
+
+            std::vector<ASGE::GLTexture *> list;
+            list.reserve(buffers.size());
+            for (auto &ptr : buffers) {
+              list.emplace_back(ptr.get());
+            }
+            return list;
+          },
+          py::return_value_policy::reference_internal, R"(
+          Returns the most recently resolved textures.
+
+          Once a MSAA texture has been resolved, it will be stored in a list
+          of resolved textures. This function returns them. If the MSAA
+          textures have been updated then these will be stale and a resolve
+          function call will be required to update them.
+
+          :returns: A list of all the currently resolved textures.
+          :type: list[Texture]
+
+          Warning
+          -------
+          You need to call resolve to sample the buffers correctly.
+
+          Example
+          -------
+            >>> """create a new render target and sprite to store the render texture"""
+            >>> self.rt = pyasge.RenderTarget(self.renderer, 1024 / 2, 768 / 2, pyasge.Texture.Format.RGB, 1)
+            >>> self.rt_texture = pyasge.Sprite()
+            >>> self.rt_texture.x = 1024 / 2
+            >>> self.rt_texture.y = 768 / 2
+            >>> self.rt_texture.width = 1024 / 2
+            >>> self.rt_texture.height = 768 / 2
+            >>> self.rt_texture.attach(self.rt.buffers[0])
+
+          See Also
+          --------
+          Texture
+      )")
+
+      .def(
+          "resolve",
+          [](ASGE::GLRenderTarget &self, int index) {
+            return self.resolve(static_cast<unsigned int>(index));
+          },
+          py::return_value_policy::reference_internal, R"(
+          Resolves the MSAA texture at specified index.
+
+          Before sampling can take place, the multi-sampled images need to be
+          "blited" in to a standard 2D texture. As render targets may have
+          multiple color attachments, this function allows the user to specify
+          which attachment to specifically update.
+
+          :param index: The index of the attachment to resolve. A count of 1 is index 0.
+          :returns: A handle to the resultant updated textures.
+          :type: Texture
+
+          Warning
+          -------
+          You need to call resolve to sample the buffers correctly.
+
+          See Also
+          --------
+          Texture
+      )")
+
+      .def(
+          "resolve",
+          [](ASGE::GLRenderTarget &self) {
+            auto &resolved = self.resolve();
+
+            std::vector<ASGE::GLTexture *> list;
+            list.reserve(resolved.size());
+            for (auto &ptr : resolved) {
+              list.emplace_back(ptr.get());
+            }
+            return list;
+          },
+          py::return_value_policy::reference_internal, R"(
+          Resolves all MSAA textures attached to this frame buffer.
+
+          Before sampling can take place, the multi-sampled images need to be
+          "blited" in to a standard 2D texture. As render targets may have
+          multiple color attachments, this function loops through all
+          attachments and resolves them into textured that can then be used in
+          normal samplers in the fragment shader.
+
+          :returns: A list of all the resultant updated texture.
+          :type: list[Texture]
+
+          Warning
+          -------
+          You need to call resolve to sample the buffers correctly.
+
+          See Also
+          --------
+          Texture
+      )");
 }
